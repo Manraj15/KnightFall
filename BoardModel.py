40170ce516ceb0bcd1f06d0eddf03f6b9a1205a3
manraj thind <manraj1999@outlook.com>
2019-02-16 11:14:20 -0500

    fix spacing and other typos

--- BoardModel.py
+++ BoardModel.py
@@ -1,14 +1,12 @@
 from random import randint
 
 class BoardModel:
-
 	board = []
 	num_points = 0
 	num_possible_moves = 0
 
 	def init(self):
 		self.makeBoard()
-
 
 	#Generate the Board using numbers randomly 
 	#generated from 1 to 4, each representing an item
@@ -19,14 +17,13 @@ class BoardModel:
 				inner_row.append(randint(1,4))
 			self.board.append(inner_row)
 		
-    #update 
     def update_num_points(self, num):
     	self.num_points = self.num_points + num
 
     def get_num(self):
     	return self.num_points
 
-	def get_board(self):
+    def get_board(self):
 		return self.board
 
 	#For debugging purposes
