7628e2dfeceb377db67c9caa6ad8ac52f2f72cbc
Yoshaan Hauzaree <yoshaan.hauzaree@mail.utoronto.ca>
2019-02-17 20:13:51 -0500

    Added function get_match, which finds identical numbers in a line around it

--- BoardModel.py
+++ BoardModel.py
@@ -28,4 +28,48 @@ class BoardModel:
 
 	#For debugging purposes
 	def print_board(self):
-		print(self.board)
+
+    def get_match(self, x, y):
+        '''
+        X=0 is the leftmost column
+        Y=0 is the rightmost row
+        Returns two variables containing the coordinates of the
+        first set of matching numbers in a line: x1, y1, x2, y2
+        if no match is foun, returns -1 -1 -1 -1
+        '''
+        #Note : this doesnt check out of bound indices yet
+        
+        #Check matches when x, y are in the center of the line of len 3
+        if self.board[y][x] == self.board[y-1][x] and self.board[y][x] == self.board[y+1][x]:
+            return x, y-1, x, y+1
+        elif self.board[y][x] == self.board[y][x-1] and self.board[y][x] == self.board[y][x+1]:
+            return x-1, y, x+1, y
+        elif self.board[y][x] == self.board[y-1][x-1] and self.board[y][x] == self.board[y+1][x+1]:
+            return x-1, y-1, x+1, y+1
+        elif self.board[y][x] == self.board[y+1][x-1] and self.board[y][x] == self.board[y-1][x+1]:
+            return x-1, y+1, x+1, y-1
+
+        #Check matches when x, y are at the edges of a line of len 3
+        for (i=x-1, i<=x+1; i++):
+            for (j=y-1; j<=y+1; j++):
+                if i!=x and j!=y and self.board[y][x] == self.board[j][i]:
+                    retX1 = i
+                    retY1 = j
+
+                    if i<x:
+                        retX2=i-1
+                    elif i=x:
+                        retX2=i
+                    else:
+                        retX2=i+1
+
+                    if j<y:
+                        retY2=j-1
+                    elif j=y:
+                        retY2=j
+                    else:
+                        retY2=j+1
+
+                    if self.board[y][x] == self.board[retY2][retX2]:
+                        return retX1, retY1, retX2, retY2
+        return -1, -1, -1, -1
