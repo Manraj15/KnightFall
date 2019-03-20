ff642aa33c9ed3b48c901e542a374bc1e288f429
Evan Gilchrist <evan.gilchrist@mail.utoronto.ca>
2019-03-20 17:36:38 -0400

    Made View work with BoardModel View now uses a board created using BoardModel and minor fixes

--- src/BoardModel.py
+++ src/BoardModel.py
@@ -6,82 +6,81 @@ Last Updated March 17, 2019
 from random import randint
 
 class BoardModel:
-	board = []
-	num_points = 0
-	num_possible_moves = 0
+        board = []
+        num_points = 0
+        num_possible_moves = 0
 
-	def init(self):
-		self.makeBoard()
+        def __init__(self):
+                self.make_board()
 
-	#Generate the Board using numbers randomly 
-	#generated from 1 to 4, each representing an item
-	def make_board(self):
-		inner_row = []
-		for i in range(0, 6):
-			for j in range(0, 6):
-				inner_row.append(randint(1,4))
-			self.board.append(inner_row)
-		
-    def update_num_points(self, num):
-    	self.num_points = self.num_points + num
+        #Generate the Board using numbers randomly 
+        #generated from 1 to 4, each representing an item
+        def make_board(self):
+                for i in range(7):
+                        inner_row = []
+                        for j in range(7):
+                                inner_row.append(randint(0,8))
+                        self.board.append(inner_row)
 
-    def get_num(self):
-    	return self.num_points
+        def update_num_points(self, num):
+                self.num_points = self.num_points + num
 
-    def get_board(self):
-		return self.board
+        def get_num(self):
+                return self.num_points
 
-	#For debugging purposes
-	def print_board(self):
+        def get_board(self):
+                return self.board
 
-    def get_match(self, x, y):
-        '''
-	Match of max len 3 in a row
-        X=0 is the leftmost column
-        Y=0 is the rightmost row
-        Returns two variables containing the coordinates of the
-        first set of matching numbers in a line: x1, y1, x2, y2
-        if no match is found, returns -1 -1 -1 -1
-        '''
-        
-        #Check matches when x, y are in the center of the line of len 3
-        if chk_range(y-1, y+1) and self.board[y][x] == self.board[y-1][x] and self.board[y][x] == self.board[y+1][x]:
-            return x, y-1, x, y+1
-        elif chk_range(x-1, x+1) and self.board[y][x] == self.board[y][x-1] and self.board[y][x] == self.board[y][x+1]:
-            return x-1, y, x+1, y
-        elif chk_range(x-1, y-1) and chk_range(y+1, x+1):
-	    if self.board[y][x] == self.board[y-1][x-1] and self.board[y][x] == self.board[y+1][x+1]:
-            return x-1, y-1, x+1, y+1
-        elif chk_range(x-1, y+1) and chk_range(x+1, y-1):
-	    if self.board[y][x] == self.board[y+1][x-1] and self.board[y][x] == self.board[y-1][x+1]:
-            return x-1, y+1, x+1, y-1
+                #For debugging purposes
 
-        #Check matches when x, y are at the edges of a line of len 3
-        for i in range(x-1, x+1):
-            for j in range(y-1, y+1):
-		
-                if i!=x and j!=y and chk_range(i,j) and self.board[y][x] == self.board[j][i]:
-		    #Assign X2 and Y2 to the next index in a line
-                    retX1 = i
-                    retY1 = j
+        def get_match(self, x, y):
+                '''
+                Match of max len 3 in a row
+                X=0 is the leftmost column
+                Y=0 is the rightmost row
+                Returns two variables containing the coordinates of the
+                first set of matching numbers in a line: x1, y1, x2, y2
+                if no match is found, returns -1 -1 -1 -1
+                '''
 
-                    if i<x:
-                        retX2=i-1
-                    elif i=x:
-                        retX2=i
-                    else:
-                        retX2=i+1
+                #Check matches when x, y are in the center of the line of len 3
+                if chk_range(y-1, y+1) and self.board[y][x] == self.board[y-1][x] and self.board[y][x] == self.board[y+1][x]:
+                        return x, y-1, x, y+1
+                elif chk_range(x-1, x+1) and self.board[y][x] == self.board[y][x-1] and self.board[y][x] == self.board[y][x+1]:
+                        return x-1, y, x+1, y
+                elif chk_range(x-1, y-1) and chk_range(y+1, x+1):
+                        if self.board[y][x] == self.board[y-1][x-1] and self.board[y][x] == self.board[y+1][x+1]:
+                                return x-1, y-1, x+1, y+1
+                elif chk_range(x-1, y+1) and chk_range(x+1, y-1):
+                        if self.board[y][x] == self.board[y+1][x-1] and self.board[y][x] == self.board[y-1][x+1]:
+                                return x-1, y+1, x+1, y-1
 
-                    if j<y:
-                        retY2=j-1
-                    elif j=y:
-                        retY2=j
-                    else:
-                        retY2=j+1
+                #Check matches when x, y are at the edges of a line of len 3
+                for i in range(x-1, x+1):
+                        for j in range(y-1, y+1):
 
-                    if chk_range(retX2, retY2) and self.board[y][x] == self.board[retY2][retX2]:
-                        return retX1, retY1, retX2, retY2
-        return -1, -1, -1, -1
+                                if i!=x and j!=y and chk_range(i,j) and self.board[y][x] == self.board[j][i]:
+                                        #Assign X2 and Y2 to the next index in a line
+                                        retX1 = i
+                                        retY1 = j
+
+                                        if i<x:
+                                                retX2=i-1
+                                        elif i==x:
+                                                retX2=i
+                                        else:
+                                                retX2=i+1
+
+                                        if j<y:
+                                                retY2=j-1
+                                        elif j==y:
+                                                retY2=j
+                                        else:
+                                                retY2=j+1
+
+                                        if chk_range(retX2, retY2) and self.board[y][x] == self.board[retY2][retX2]:
+                                                return retX1, retY1, retX2, retY2
+                return -1, -1, -1, -1
 
 def chk_range(x, y):
-	return (x>=0 and x<=6 and y>=0 and y<=6)
+        return (x>=0 and x<=6 and y>=0 and y<=6)
--- src/View.py
+++ src/View.py
@@ -6,60 +6,74 @@ Last updated in March 17 2019
 import pygame
 import os
 from random import randint
+from PauseMenu import *
+from Button import *
+from BoardModel import *
 
 class view:
     
-    window_width = 1000
-    window_height = 800
+    WINDOW_WIDTH = 1200
+    WINDOW_HEIGHT = 800
     ICONSIZE = 80
+    BACKGROUND_COLOUR_1 = (49, 142, 105)
+    BACKGROUND_COLOUR_2 = (48, 115, 221)
+    board_model = None
+    
     def __init__(self):
         '''
-        Creates a visual representation of the 
+        (View) -> None
+        Creates a visual representation of the
         KnightFall game that was started
         '''
+        self.load_board()
         self.draw_gameboard()
+        
         
     def draw_gameboard(self):
         '''
+        (View) -> None
         sets up the visual representation of the 
         game board the user will play on
         '''
         pygame.init()
         
-        screen = pygame.display.set_mode([self.window_width, self.window_height])
-        screen.fill((49, 142, 105))
+        screen = pygame.display.set_mode([self.WINDOW_WIDTH, self.WINDOW_HEIGHT])
+        screen.fill(self.BACKGROUND_COLOUR_1)
         
         font = pygame.font.Font(None, 50)
         
-        # creating a pause icon
-        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(50, 35, 140, 60))
-        pygame.draw.rect(screen, (49, 142, 105), pygame.Rect(55, 40, 130, 50))
-        pause_title = font.render("Pause", 1, (255, 255, 255))
-        screen.blit(pause_title, (60, 50))
+        # creating a pause button
+        pause_button = Button('Pause', self.path_to_image('imgs\\gui-play\\btn-pause.png'), screen, [10,10], None)
+        pause_button.display_button()
         
-        score_title = font.render("Score", 1, (255, 255, 255))
-        screen.blit(score_title, (50, 550))
-        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(50, 590, 200, 50))
-        pygame.draw.rect(screen, (49, 142, 105), pygame.Rect(55, 595, 190, 40))
+        # create score title and area for the score to go
+        score_title = Button('Score', self.path_to_image('imgs\\gui-play\\btn-score.png'), screen, [10,450], None)
+        score_title.display_button()
+        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(50, 530, 310, 70))
+        pygame.draw.rect(screen, self.BACKGROUND_COLOUR_1, pygame.Rect(55, 535, 300, 60))        
         
-        time_title = font.render("Time Left", 1, (255, 255, 255))
-        screen.blit(time_title, (50, 650))
-        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(50, 690, 200, 50))
-        pygame.draw.rect(screen, (49, 142, 105), pygame.Rect(55, 695, 190, 40))
+        # create timer title and area for the timer to go
+        time_title = Button('Time Remaining', self.path_to_image('imgs\\gui-play\\btn-timer.png'), screen, [10,650], None)
+        time_title.display_button()
+        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(50, 730, 310, 70))
+        pygame.draw.rect(screen, self.BACKGROUND_COLOUR_1, pygame.Rect(55, 735, 300, 60))
+        
         #load image
         KNIGHICONS = []
         for i in range(1,10):
-            knightimg = pygame.image.load(os.path.join(os.path.abspath('')[:-4], 'imgs\\Medieval_%d.png' % i)).convert_alpha()
+            knightimg = pygame.image.load(self.path_to_image('imgs\\Medieval_%d.png' % i)).convert_alpha()
             if knightimg.get_size() != (self.ICONSIZE, self.ICONSIZE):
                 knightimg = pygame.transform.smoothscale(knightimg, (self.ICONSIZE, self.ICONSIZE))
             KNIGHICONS.append(knightimg)
+            
         # making the part of the board containing the icons a different colour
-        pygame.draw.rect(screen, (48, 115, 221), pygame.Rect(300, 0, 700, 800))
+        pygame.draw.rect(screen, self.BACKGROUND_COLOUR_2, pygame.Rect(550, 0, self.WINDOW_WIDTH - 500, self.WINDOW_HEIGHT))
         
-        #this currently just randomly the icons on the screen, however it should get them from an instance of BoardModel
+        #this displays the items in BoardModel board_model on the screen
         for col in range(7):
             for row in range(7):
-                screen.blit(KNIGHICONS[randint(0, 8)], (365 + 90 * col, 80 + 90 * row)) 
+                screen.blit(KNIGHICONS[self.board_model.board[row][col]], (self.WINDOW_WIDTH - 635 + 90 * col, self.WINDOW_HEIGHT - 720 + 90 * row)) 
+                
         # keeps the board open until the user closes it
         closed = False
         while not closed:
@@ -69,3 +83,21 @@ class view:
             pygame.display.flip()
             
         pygame.quit()
+        
+    def path_to_image(self, image_location):
+        '''
+        (View, str) -> str
+        Creates and returns the file path to the location
+        designated by image_location with the starting
+        directory being KnightFall
+        '''        
+        return os.path.join(os.path.abspath('')[:-4], image_location)
+    
+    def load_board(self):
+        '''
+        (View) -> None
+        Sets the current instance of BoardModel that
+        will be used in this instance of the view
+        in board_model
+        '''
+        self.board_model = BoardModel()
